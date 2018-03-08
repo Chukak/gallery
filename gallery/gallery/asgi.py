340c2799e9deaf49e1ae7393c78b99e2998e41e6
chukak <chukak12@gmail.com>
2018-03-08 14:42:36 +0300

    Fix setting asgi.py

--- gallery/gallery/asgi.py
+++ gallery/gallery/asgi.py
@@ -3,7 +3,6 @@ import django
 from channels.routing import get_default_application
 
 # setting channels
-# check https://channels.readthedocs.io/en/latest/
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gallery.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gallery.settings.development")
 django.setup()
 application = get_default_application()
