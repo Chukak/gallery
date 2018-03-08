0f99e655a8463413f0778d51dff7a13a6d12f6c7
chukak <chukak12@gmail.com>
2018-03-08 14:39:09 +0300

    Create tests. Add test for image path with uuid

--- gallery/image_gallery/tests.py
+++ gallery/image_gallery/tests.py
@@ -1,3 +1,30 @@
+import os
+import re
 from django.test import TestCase
+from .models import get_image_path
 
-# Create your tests here.
+
+class ImagePathTest(TestCase):
+    def test_image_path(self):
+        """
+        Test image path in the format images/[random uuid].[file extension]
+        (for example, images/f278c317-a946-4802-868e-42ddde3d3f56.png, etc).
+
+        test_list - list of files in the format [image name].[file extension]
+        (for example, image.jpg, image.png, etc).
+
+        """
+        test_list = [
+            "image.jpg",
+            "picture.png",
+            "random_image.jpeg",
+        ]
+        for pattern in test_list:
+            uuid_image = get_image_path(None, pattern)
+            file_extension = os.path.splitext(pattern)[1]
+            regex = re.compile(
+                '^images/[0-9A-F]{{8}}-[0-9A-F]{{4}}-4[0-9A-F]{{3}}-'
+                '[89AB][0-9A-F]{{3}}-[0-9A-F]{{12}}{extension}$'.format(extension="\\" + file_extension),
+                flags=re.IGNORECASE
+            )
+            self.assertTrue(re.fullmatch(regex, uuid_image))
